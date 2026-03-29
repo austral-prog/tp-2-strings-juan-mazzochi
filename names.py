1481ffd8e8d7be6b0c1e54aeec31c86951a3ccb9
juan-mazzochi <juanmartinmazzochi@gmail.com>
2026-03-29 16:37:07 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,7 +2,6 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
     nombre = input("Ingrese nombre: ")
     apellido = input("Ingrese apellido: ")
     print(nombre.lower() + " " + apellido.lower())
